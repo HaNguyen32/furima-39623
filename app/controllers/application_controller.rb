6bf865732d506ae5dbcf6d54a90c3678c7b4f29f
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-26 21:17:40 +0900

    ログインとログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :katagana_first_name, :katagana_last_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :last_name, :katagana_first_name, :katagana_last_name, :date_of_birth])
   end
 end
