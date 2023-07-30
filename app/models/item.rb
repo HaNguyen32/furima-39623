3a00e976c75b8a0d61f5bfe4cf947042ff682548
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-30 17:31:06 +0900

    カテゴリー選択など設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,25 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_one_attached :image
+    
+    extend ActiveHash::Associations::ActiveRecordExtensions 
+    belongs_to :category
+    belongs_to :sales_status
+    belongs_to :shipping_fee_status 
+    belongs_to :prefecture
+    belongs_to :scheduled_delivery
+    
 
 
     with_options presence: true do
+        validates :image
         validates :name
-        validates :category_id
-        validates :sales_status_id
-        validates :shipping_fee_status_id
-        validates :prefecture_id
-        validates :schuduled_delivery_id
-        validates :price
+        validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+        validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+        validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+        validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+        validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+        validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Price is out of setting range" }
     end
+
 end
