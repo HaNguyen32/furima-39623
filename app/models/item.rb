7de1e66c627976af8a774f20432850b2e37c9224
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-31 15:56:12 +0900

    バリエーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
         validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
         validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
         validates :price, numericality: {
-            only_integer: { message: "is invalid. Input half-width characters" },
+            only_integer: true, message: "is invalid. Input half-width characters" ,
             greater_than_or_equal_to: 300,
             less_than_or_equal_to: 9999999,
             message: "is out of setting range"
