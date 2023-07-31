daa84be024df627e3e95bbc9351dfe4931edc8fb
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-31 13:06:00 +0900

    itemモデルのテストコード実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,12 +14,17 @@ class Item < ApplicationRecord
     with_options presence: true do
         validates :image
         validates :name
+        validates :info
         validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
         validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
         validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
         validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
         validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-        validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Price is out of setting range" }
+        validates :price, numericality: {
+            only_integer: { message: "is invalid. Input half-width characters" },
+            greater_than_or_equal_to: 300,
+            less_than_or_equal_to: 9999999,
+            message: "is out of setting range"
+          }
     end
-
 end
