b90411036331ffe49b4b4a8e2510fe7e8c1c0312
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-07-31 20:23:10 +0900

    showアクションをcontrollerに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,32 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
-    def index
-       @items = Item.all.order("created_at DESC")
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @item = Item.new
-    end
+  def show
+    @item = Item.find(params[:id])
+  end
 
-    def create
-        @item = Item.new(item_params)
-        if @item.valid?
-          @item.save
-          redirect_to root_path
-        else
-          render :new, status: :unprocessable_entity
-        end
-    end
-    
-    private
+  private
 
-    def item_params
-        params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-    end
-
-
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id,
+                                 :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
 end
