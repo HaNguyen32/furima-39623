0a1065b8296b51032644c574da7aabeae36ce3fa
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-08-04 21:18:29 +0900

    不要なコメントアウトを削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,18 +5,14 @@ class OrdersController < ApplicationController
 
     def index
         gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-        #@order = Item.find(params[:id])
-        @item = Item.find(params[:item_id])
         @order_address = OrderAddress.new
     end
 
     def create
-        @item = Item.find(params[:item_id])
         @order_address = OrderAddress.new(order_params) 
         if @order_address.valid?
           pay_item
           @order_address.save
-          #@order_address.item.update(sold_out: true)
           redirect_to root_path
         else
           render :index, status: :unprocessable_entity
@@ -26,7 +22,6 @@ class OrdersController < ApplicationController
     private
 
     def order_params
-        #params.require(:order_address).permit( :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :item_id).merge(user_id: current_user.id)
         params.require(:order_address).permit( :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id).merge(item_id: @item.id).merge(user_id: current_user.id).merge(token: params[:token])
     end
 
@@ -41,7 +36,6 @@ class OrdersController < ApplicationController
 
     def move_to_item_index
       @item = Item.find(params[:item_id])
-      @order_address = OrderAddress.new
       if current_user == @item.user
         redirect_to root_path
       elsif !@item.order.nil?
