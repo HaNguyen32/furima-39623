68103af2575a69281703cfeb6248611cce95b5f0
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-08-04 12:10:50 +0900

    phone_numberのValidatesを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,8 +7,8 @@ class OrderAddress
       validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
       validates :city
       validates :house_number
-      validates :phone_number, numericality: { only_integer: true },
-                               length: { minimum: 10, maximum: 11 }
+      validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' },
+                               length: { minimum: 10, message: 'is too short' }
       validates :user_id
       validates :item_id
       #validates :order_id  
