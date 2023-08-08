3db1d8a6a7b9ef5e6c0404184fb7ab07e28eb5e3
HaNguyen32 <juhi_nguyen.hfl@outlook.jp>
2023-08-08 20:15:40 +0900

    deploy.rbの編集

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.17.3'
+
+set :application, 'furima-39623'
+set :repo_url, 'git@example.com:HaNguyen32/furima-39623.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' 
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/juhi.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
